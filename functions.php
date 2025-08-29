8c26a97ee6fd1ad50f61a0bd2fe160f1f90fa0c6
davidag90 <info@dagdev.com.ar>
2025-08-29 09:45:11 -0300

    Feat: new sidebar registered for "Refugios" section

--- functions.php
+++ functions.php
@@ -153,6 +153,18 @@ function armch_widgets_init()
 			'after_title'   => '</h2>',
 		)
 	);
+
+	register_sidebar(
+		array(
+			'name'          => 'Refugios',
+			'id'            => 'refugios',
+			'description'   => 'Widgets para la sección de Refugios',
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action('widgets_init', 'armch_widgets_init');
 
